2e7afe7f96232a793cc2633c2a632ebd0ca788ea
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-11-07 18:43:47 +0100

    added tests

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -3,5 +3,23 @@ import RHBCoreData
 
 final class RHBCoreDataTests: XCTestCase {
     func testRHBCoreData() {
+        let model = NSManagedObjectModel(name: "Model", in: Bundle(for: type(of: self)))!
+        let container = NSPersistentContainer(name: "test", managedObjectModel: model)
+        XCTAssert(container.persistentStoreDescriptions.count == 1)
+        container.persistentStoreDescriptions.forEach { store in
+            try! container.persistentStoreCoordinator.destroyPersistentStore(at: store.url!, ofType: store.type, options: nil)
+        }
+        container.loadPersistentStores { desc, err in
+            print(desc)
+            XCTAssertNil(err)
+        }
+        let fetchRequest = makeFetchRequest(TestEntity.self)
+        XCTAssert(try! container.viewContext.fetch(fetchRequest).isEmpty)
+        let testEntity = TestEntity(context: container.viewContext)
+        try! container.viewContext.save()
+        XCTAssert(try! container.viewContext.fetch(fetchRequest).count == 1)
+        testEntity.delete()
+        try! container.viewContext.save()
+        XCTAssert(try! container.viewContext.fetch(fetchRequest).isEmpty)
     }
 }
