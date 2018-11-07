045547072a32aac0bd206bc3c7c60a5b6319b0b3
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-11-07 17:23:11 +0100

    fixed extension

--- Sources/NSFetchedResultsController.swift
+++ Sources/NSFetchedResultsController.swift
@@ -1,6 +1,6 @@
 import CoreData
 
-public extension NSFetchedResultsController {
+@objc public extension NSFetchedResultsController {
     convenience init?(performing request: NSFetchRequest<ResultType>, in context: NSManagedObjectContext, section: String? = nil, cache: String? = nil) {
         self.init(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: section, cacheName: cache)
         do {
