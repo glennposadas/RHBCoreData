9540d77db696d123ffd4d52fda8eadfcfdae5a64
Lazar Otasevic <lazar.otasevic@1und1.de>
2018-11-07 19:14:32 +0100

    test

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -2,13 +2,18 @@ import XCTest
 import RHBCoreData
 
 final class RHBCoreDataTests: XCTestCase {
-    func testRHBCoreData() {
-        let model = NSManagedObjectModel(name: "Model", in: Bundle(for: type(of: self)))!
-        let container = NSPersistentContainer(name: "test", managedObjectModel: model)
+    static let model = NSManagedObjectModel(name: "Model", in: Bundle(for: RHBCoreDataTests.self))!
+    var container: NSPersistentContainer!
+
+    override func setUp() {
+        container = NSPersistentContainer(name: "test", managedObjectModel: RHBCoreDataTests.model)
         XCTAssert(container.persistentStoreDescriptions.count == 1)
         container.persistentStoreDescriptions.forEach { store in
             try! container.persistentStoreCoordinator.destroyPersistentStore(at: store.url!, ofType: store.type, options: nil)
         }
+    }
+
+    func testBasicCoreData() {
         container.loadPersistentStores { desc, err in
             print(desc)
             XCTAssertNil(err)
@@ -21,5 +26,29 @@ final class RHBCoreDataTests: XCTestCase {
         testEntity.delete()
         try! container.viewContext.save()
         XCTAssert(try! container.viewContext.fetch(fetchRequest).isEmpty)
+    }
+
+    func testCoreDataAsyncOk() {
+        let ex = self.expectation(description: "async")
+        container.persistentStoreDescriptions.append(NSPersistentStoreDescription(url: URL(fileURLWithPath: "/tmp/xxx1.sqlite")))
+        container.persistentStoreDescriptions.append(NSPersistentStoreDescription(url: URL(fileURLWithPath: "/tmp/xxx2.sqlite")))
+        XCTAssert(container.persistentStoreDescriptions.count == 3)
+        container.loadPersistentStoresAsync { errors in
+            XCTAssert(errors.isEmpty)
+            ex.fulfill()
+        }
+        self.waitForExpectations(timeout: 2, handler: nil)
+    }
+
+    func testCoreDataAsyncErrors() {
+        let ex = self.expectation(description: "async")
+        container.persistentStoreDescriptions.append(NSPersistentStoreDescription(url: URL(fileURLWithPath: "/verybadpath/xxx1.sqlite")))
+        container.persistentStoreDescriptions.append(NSPersistentStoreDescription(url: URL(fileURLWithPath: "/verybadpath/xxx2.sqlite")))
+        XCTAssert(container.persistentStoreDescriptions.count == 3)
+        container.loadPersistentStoresAsync { errors in
+            XCTAssert(errors.count == 2)
+            ex.fulfill()
+        }
+        self.waitForExpectations(timeout: 2, handler: nil)
     }
 }
