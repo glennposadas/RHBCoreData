9552e4c0ab7f4efd06273c4217f8f9691afff580
Lazar Otasevic <redhotbits@gmail.com>
2018-12-31 11:27:35 +0100

    some new methods

--- Sources/NSManagedObjectContext.swift
+++ Sources/NSManagedObjectContext.swift
@@ -1,16 +1,31 @@
 import CoreData
 
 public extension NSManagedObjectContext {
+    func fetchMore<T: NSManagedObject>(_ request: NSFetchRequest<T>, _ block: ((Error) -> Void)? = nil) -> [T]? {
+        do {
+            return try fetch(request)
+        } catch {
+            block?(error)
+        }
+        return nil
+    }
+
+    func fetchOne<T: NSManagedObject>(_ request: NSFetchRequest<T>, _ block: ((Error) -> Void)? = nil) -> T? {
+        return fetchMore(request, block)?.first
+    }
+
     func performTaskAndWait(_ block: @escaping (NSManagedObjectContext) -> Void) {
         performAndWait {
             block(self)
         }
     }
+
     func performTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
         perform { [unowned self] in
             block(self)
         }
     }
+
     func performResult<T>(_ block: @escaping (NSManagedObjectContext) -> T) -> T {
         var t: T!
         performAndWait {
@@ -18,16 +33,21 @@ public extension NSManagedObjectContext {
         }
         return t
     }
-    func save(_ block: (Error)->Void) {
+
+    @discardableResult
+    func saveChanges(_ block: ((Error)->Void)? = nil) -> Bool {
         guard hasChanges else {
-            return
+            return true
         }
         do {
             try save()
+            return true
         } catch {
-            block(error)
+            block?(error)
+            return false
         }
     }
+
     func reloadObject<T: NSManagedObject>(other: T) -> T? {
         let baseObject = try? existingObject(with: other.objectID)
         return baseObject as? T
