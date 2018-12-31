3e596b624a152fc30f8da60cb5c9459b4536296d
Lazar Otasevic <redhotbits@gmail.com>
2018-12-31 16:53:31 +0100

    added fetch request builder

--- Sources/NSFetchRequest.swift
+++ Sources/NSFetchRequest.swift
@@ -3,3 +3,34 @@ import CoreData
 public func genericFetchRequest<T: NSManagedObject>(_ t: T.Type) -> NSFetchRequest<T> {
     return T.fetchRequest() as! NSFetchRequest<T>
 }
+
+public class FetchRequestBuilder<T: NSManagedObject> {
+    public let request = genericFetchRequest(T.self)
+}
+
+public extension FetchRequestBuilder {
+    func fetch(limit: Int) -> FetchRequestBuilder<T> {
+        request.fetchLimit = limit
+        return self
+    }
+
+    func and(predicate: NSPredicate) -> FetchRequestBuilder<T> {
+        request.predicate = request.predicate.map { NSCompoundPredicate(andPredicateWithSubpredicates:[$0, predicate]) } ?? predicate
+        return self
+    }
+
+    func or(predicate: NSPredicate) -> FetchRequestBuilder<T> {
+        request.predicate = request.predicate.map { NSCompoundPredicate(orPredicateWithSubpredicates:[$0, predicate]) } ?? predicate
+        return self
+    }
+
+    func notPredicate() -> FetchRequestBuilder<T> {
+        request.predicate = request.predicate.map { NSCompoundPredicate(notPredicateWithSubpredicate: $0) }
+        return self
+    }
+
+    func sort(descriptor: NSSortDescriptor) -> FetchRequestBuilder<T> {
+        request.sortDescriptors = (request.sortDescriptors ?? []) + [descriptor]
+        return self
+    }
+}
