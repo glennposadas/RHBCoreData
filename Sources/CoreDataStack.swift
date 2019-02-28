9062cb66f35060887a5f0336ac1c924b8c67d7bc
Lazar Otasevic <redhotbits@gmail.com>
2019-02-28 23:07:14 +0100

    improved coredata stack

--- Sources/CoreDataStack.swift
+++ Sources/CoreDataStack.swift
@@ -2,19 +2,21 @@ import CoreData
 import RHBFoundation
 
 open class CoreDataStack {
-    public let mainContext: NSManagedObjectContext
-    public let writingContext: BackgroundManagedObjectContext
-    public let readingContext: BackgroundManagedObjectContext
+    public let persistentContainer: NSPersistentContainer
 
+    public private(set) lazy var mainContext = persistentContainer.viewContext ~ {
+        $0.automaticallyMergesChangesFromParent = true
+    }
+
+    public private(set) lazy var writingContext = BackgroundManagedObjectContext(persistentContainer) {
+        $0.automaticallyMergesChangesFromParent = false
+    }
+
+    public private(set) lazy var readingContext = BackgroundManagedObjectContext(persistentContainer) {
+        $0.automaticallyMergesChangesFromParent = true
+    }
+    
     public init(_ persistentContainer: NSPersistentContainer) {
-        self.mainContext = persistentContainer.viewContext ~ {
-            $0.automaticallyMergesChangesFromParent = true
-        }
-        self.writingContext = BackgroundManagedObjectContext(persistentContainer) {
-            $0.automaticallyMergesChangesFromParent = false
-        }
-        self.readingContext = BackgroundManagedObjectContext(persistentContainer) {
-            $0.automaticallyMergesChangesFromParent = true
-        }
+        self.persistentContainer = persistentContainer
     }
 }
