0b9d2cf7097a3c16f5292c83932454ca4976d594
Lazar Otasevic <redhotbits@gmail.com>
2019-03-01 20:36:23 +0100

    add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RHBCoreData",
+    platforms: [
+        .macOS(.v10_12), .iOS("10.3")
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
