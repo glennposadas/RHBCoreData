924d0dcfb9d7f31143d05e40b2bff8c7d2681370
Lazar Otasevic <redhotbits@gmail.com>
2019-03-02 01:01:18 +0100

    package added rhbxctest

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/sisoje/RHBFoundation", .branch("master")),
+        .package(url: "https://github.com/sisoje/RHBXCTest", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,7 @@ let package = Package(
             path: "Sources"),
         .testTarget(
             name: "RHBCoreDataTests",
-            dependencies: ["RHBCoreData"],
+            dependencies: ["RHBCoreData", "RHBXCTest"],
             path: "Tests"),
     ]
 )
