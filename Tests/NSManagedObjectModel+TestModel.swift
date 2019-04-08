0d11e844982f6f309737f6c386927f30562c9c57
Lazar Otasevic <redhotbits@gmail.com>
2019-04-08 21:15:34 +0200

    messing arround with entity

--- Tests/NSManagedObjectModel+TestModel.swift
+++ Tests/NSManagedObjectModel+TestModel.swift
@@ -9,10 +9,12 @@ extension NSManagedObjectModel {
                 $0.managedObjectClassName = String(reflecting: TestEntity.self)
                 $0.properties = [
                     NSAttributeDescription() ~ {
+                        $0.isOptional = true
                         $0.name = #keyPath(TestEntity.id)
                         $0.attributeType = .stringAttributeType
                     },
                     NSAttributeDescription() ~ {
+                        $0.isOptional = true
                         $0.name = #keyPath(TestEntity.text)
                         $0.attributeType = .stringAttributeType
                     },
--- Tests/TestEntity.swift
+++ Tests/TestEntity.swift
@@ -1,6 +1,6 @@
 import CoreData
 
 class TestEntity: NSManagedObject {
-    @NSManaged public var id: String!
-    @NSManaged public var text: String!
+    @NSManaged public var id: String?
+    @NSManaged public var text: String?
 }
