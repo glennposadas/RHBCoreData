b449171161b971914e17e2c85fd45b28e1d7eab7
Lazar Otasevic <redhotbits@gmail.com>
2019-04-08 16:04:38 +0200

    renamed

--- Sources/NSManagedObjectModel.swift
+++ Sources/NSManagedObjectModel.swift
@@ -1,8 +1,8 @@
 import CoreData
 
 public extension NSManagedObjectModel {
-    convenience init?(name: String, in bundle: Bundle = .main) {
-        guard let url = bundle.url(forResource: name, withExtension: "momd") else {
+    convenience init?(modelDocumentName: String, bundle: Bundle = .main) {
+        guard let url = bundle.url(forResource: modelDocumentName, withExtension: "momd") else {
             return nil
         }
         self.init(contentsOf: url)
