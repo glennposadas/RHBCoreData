68c85aa6c089a7f6308fa29f5b11c8da989b8bab
Lazar Otasevic <redhotbits@gmail.com>
2019-04-09 13:22:13 +0200

    add objc

--- Tests/TestEntity.swift
+++ Tests/TestEntity.swift
@@ -1,6 +1,6 @@
 import CoreData
 
-class TestEntity: NSManagedObject {
+@objc class TestEntity: NSManagedObject {
     @NSManaged public var id: String?
     @NSManaged public var text: String?
     @NSManaged public var deca: Decimal
