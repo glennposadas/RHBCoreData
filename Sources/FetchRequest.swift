4c7a52a8b6d913c28a850ce74f7a579938402a22
Lazar Otasevic <redhotbits@gmail.com>
2019-04-10 12:03:11 +0200

    try to fix something

--- Sources/FetchRequest.swift
+++ Sources/FetchRequest.swift
@@ -1,4 +1,5 @@
 import CoreData
+import RHBFoundation
 
 public enum FetchRequest<T: NSManagedObject> {}
 
@@ -7,5 +8,42 @@ public extension FetchRequest {
         // swiftlint:disable force_cast
         return T.fetchRequest() as! NSFetchRequest<T>
         // swiftlint:enable force_cast
+    }
+}
+
+public class FetchRequestBuilder<T: NSManagedObject> {
+    public let request = FetchRequest<T>.request
+}
+
+public extension FetchRequestBuilder {
+    convenience init<P: NSPredicate & TypedPredicateProtocol>(predicate: P) where P.Root == T {
+        self.init()
+        self.request.predicate = predicate
+    }
+
+    convenience init<V: Comparable>(sortBy keyPath: KeyPath<T, V?>, ascending: Bool) {
+        self.init()
+        addSort(by: keyPath, ascending: ascending)
+    }
+
+    convenience init<V: Comparable>(sortBy keyPath: KeyPath<T, V>, ascending: Bool) {
+        self.init()
+        addSort(by: keyPath, ascending: ascending)
+    }
+
+    func addSort<V: Comparable>(by keyPath: KeyPath<T, V?>, ascending: Bool) {
+        request.addsort(NSSortDescriptor(keyPath: keyPath, ascending: ascending))
+    }
+
+    func addSort<V: Comparable>(by keyPath: KeyPath<T, V>, ascending: Bool) {
+        request.addsort(NSSortDescriptor(keyPath: keyPath, ascending: ascending))
+    }
+}
+
+// MARK: - internal
+
+@objc extension NSFetchRequest {
+    func addsort(_ desc: NSSortDescriptor) {
+        sortDescriptors = (sortDescriptors ?? []) + [desc]
     }
 }
